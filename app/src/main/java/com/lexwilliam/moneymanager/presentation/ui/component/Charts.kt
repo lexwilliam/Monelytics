becb4b45e92289e5c435ae3c5909eee0838a7740
Chun2Maru <57833134+Chun2Maru@users.noreply.github.com>
2021-08-02 22:12:22 +0700

    make a chart prototype

--- app/src/main/java/com/lexwilliam/moneymanager/presentation/ui/component/Charts.kt
+++ app/src/main/java/com/lexwilliam/moneymanager/presentation/ui/component/Charts.kt
@@ -1,90 +1,42 @@
 package com.lexwilliam.moneymanager.presentation.ui.component
 
-import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
-import com.lexwilliam.moneymanager.data.model.ReportType
-import com.lexwilliam.moneymanager.presentation.model.ReportPresentation
-import com.lexwilliam.moneymanager.presentation.util.fakeReports
-import com.lexwilliam.moneymanager.presentation.util.formatDateToString
+import androidx.compose.ui.viewinterop.AndroidView
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 
-//
-//@Composable
-//fun BarChart(
-//    reports: List<ReportPresentation>
-//) {
-//    val groupedReports = reports.groupBy { formatDateToString(it.timeAdded!!, "MMM") }
-//
-//    data class MonthAndBalance(val month: String, var income: Double, var expense: Double)
-//    val monthAndBalanceList = mutableListOf<MonthAndBalance>()
-//    var maxBalance = 0.0
-//    groupedReports.forEach { (month, reports) ->
-//        val balance = MonthAndBalance(month, 0.0, 0.0)
-//        reports.forEach { report ->
-//            when(report.reportType) {
-//                ReportType.Income -> balance.income += report.money
-//                ReportType.Expense -> balance.expense -= report.money
-//                else -> Log.d("BarChart", "Money Error")
-//            }
-//        }
-//        if(balance.income > maxBalance) {
-//            maxBalance = balance.income
-//        }
-//        if(balance.expense > maxBalance) {
-//            maxBalance = -(balance.expense)
-//        }
-//        monthAndBalanceList.add(balance)
-//    }
-//
-//    Column {
-//        Row(
-//            modifier = Modifier.height(100.dp).fillMaxWidth(),
-//            horizontalArrangement = Arrangement.spacedBy(8.dp),
-//            verticalAlignment = Alignment.Bottom
-//        ) {
-//            monthAndBalanceList.forEach { monthAndBalance ->
-//                val incomeHeightPercentage = monthAndBalance.income / maxBalance
-//                val expenseHeightPercentage = monthAndBalance.expense / maxBalance
-//                Row(
-//                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-//                    verticalAlignment = Alignment.Bottom
-//                ) {
-//                    Box(
-//                        modifier = Modifier
-//                            .background(Color.Green)
-//                            .fillMaxHeight(incomeHeightPercentage.toFloat())
-//                            .width(24.dp)
-//                    )
-//                    Box(
-//                        modifier = Modifier
-//                            .background(Color.Red)
-//                            .fillMaxHeight(expenseHeightPercentage.toFloat())
-//                            .width(24.dp)
-//                    )
-////                    Box(
-////                        modifier = Modifier
-////                            .weight(1f),
-////                        contentAlignment = Alignment.Center
-////                    ) {
-////                        Text(text = monthAndBalance.month, style = MaterialTheme.typography.subtitle2, color = Color.White)
-////                    }
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//
-//
-//@Preview
-//@Composable
-//fun BarChartPreview() {
-//    BarChart(reports = fakeReports)
-//}
+@Composable
+fun MyBarChart() {
+    AndroidView(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+            .padding(horizontal = 24.dp),
+        factory = { context ->
+            BarChart(context)
+        },
+        update = {
+            it.apply {
+                val entries = mutableListOf<BarEntry>()
+                entries.add(BarEntry(1.0f, 10.0f))
+                entries.add(BarEntry(2.0f, 3.0f))
+                entries.add(BarEntry(3.0f, 4.0f))
+                entries.add(BarEntry(4.0f, 7.0f))
+                val dataSet = BarDataSet(entries, "Month")
+                data = BarData(dataSet)
+                description = null
+                isLogEnabled = true
+                notifyDataSetChanged()
+                invalidate()
+            }
+        }
+    )
+}
